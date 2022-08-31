8a6b66a2bd1e9ee02dc5cdf6cc217c0fde8fb923
joaopedro2602 <joao.cabral14@etec.sp.gov.br>
2022-08-31 14:24:43 -0300

    segunda versão

---
+++ app/src/main/java/com/example/apprevisao3/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.apprevisao3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val edtNome : EditText = findViewById(R.id.edtNome)
+        val edtEndereco : EditText = findViewById(R.id.edtEndereco)
+        val edtBairro : EditText = findViewById(R.id.edtBairro)
+        val edtCep : EditText = findViewById(R.id.edtCep)
+
+        val btnCadastrar : Button = findViewById(R.id.btnCadastrar)
+
+        btnCadastrar.setOnClickListener {
+            val intent = Intent(this,Resultado::class.java)
+            intent.putExtra("nome",edtNome.text.toString())
+            intent.putExtra("endereco",edtEndereco.text.toString())
+            intent.putExtra("cep",edtCep.text.toString())
+            intent.putExtra("bairro",edtBairro.text.toString())
+            startActivity(intent)
+        }
+    }
+}
