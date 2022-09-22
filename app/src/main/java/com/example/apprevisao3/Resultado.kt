fc7cfd1c3fc9f4c59bb90c67f9a04701afefb16b
joaopedro2602 <joao.cabral14@etec.sp.gov.br>
2022-09-22 14:26:45 -0300

    terceira versao

---
+++ app/src/main/java/com/example/apprevisao3/Resultado.kt
@@ -0,0 +1,38 @@
+package com.example.apprevisao3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.example.apprevisao.db.DBHelper
+
+
+class Resultado : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+
+        val edtNome : EditText = findViewById(R.id.edtNome)
+        val edtEndereco : EditText = findViewById(R.id.edtEndereco)
+        val edtBairro : EditText = findViewById(R.id.edtBairro)
+        val edtCep : EditText = findViewById(R.id.edtCep)
+
+        val btnConfirmar : Button = findViewById(R.id.btnConfirmar)
+
+        edtNome.setText(intent.getStringExtra("nome"))
+        edtEndereco.setText(intent.getStringExtra("endereco"))
+        edtCep.setText(intent.getStringExtra("cep"))
+        edtBairro.setText(intent.getStringExtra("bairro"))
+
+
+        val db = DBHelper(this, null)
+
+        btnConfirmar.setOnClickListener{
+            db.addPessoa(edtNome.text.toString(), edtEndereco.text.toString(),
+                edtCep.text.toString(), edtBairro.text.toString())
+            finish()
+        }
+
+
+    }
+}
